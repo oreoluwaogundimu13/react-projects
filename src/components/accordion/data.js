4b5278251724d66c4289aac8c620ab71a85ac4e9
oreoluwa ogundimu <oreoluwaogundimu19@gmail.com>
2024-09-13 10:16:44 +0100

    Accordion,random-color,star-rating

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : '1',
+        question: "What are accordion components?",
+        answer: "Accordion components are collapsible panels that can be used to display content in a compact"
+    },
+    {
+        id : '2',
+        question: "What are they used for?",
+        answer: "Accordion components are used to display content in a compact way, and can be used to display information in a way that is easy to read and understand.",
+    },
+    {
+        id : '3',
+        question: "How do they work?",
+        answer: "Accordion components work by allowing the user to click on a title or header to expand",
+    },
+    {
+        id : '4',
+        question: "What are the benefits of using accordion components?",
+        answer: "Accordion components are beneficial because they allow users to easily view and interact with content in a compact way.",
+    },
+];
+
+export default data;
